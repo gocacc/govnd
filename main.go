09c950c681db66f7a216339178dd02cf1174fd0d
gocacc <2548171533@qq.com>
2023-04-24 20:43:25 +0800

    20230424

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"utils/goexecl"
+)
+
+func main() {
+	al := goexecl.Execl("/mnt/d/Users/gocacc/Desktop/图纸/云山监控IP.xlsx")
+}
---
+++ utils/goexecl_test.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestExecl(t *testing.T) {
+	Execl("/mnt/d/Users/gocacc/Desktop/图纸/云山监控IP.xlsx")
+}
